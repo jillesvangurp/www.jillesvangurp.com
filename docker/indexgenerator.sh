325f26d562bda1cd3589b5dda8e0c662f83fee92
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-12-25 13:22:54 +0100

    fix readme extract blog index header from script

--- docker/indexgenerator.sh
+++ docker/indexgenerator.sh
@@ -1,18 +1,6 @@
 #! /usr/bin/env bash
 rm -f _index.md
-export HEADER=`cat <<EOF
----
-title: Article Index
-author: Jilles van Gurp
----
-
-I used to have a wordpress blog and before that, I used something called pivot. These days I use a simple static site generator (pandoc) with some home grown scripts and templates. I tried to preserve the articles and link structure when I migrated the content to markdown. There is about 2 decades worth of content here. Most of it no longer relevant of course. But I decided to preserve it.
-
-In recent years, I've published articles on platforms like [medium](https://medium.com/@jillesvangurp_30276) and [dev.to](https://dev.to/jillesvangurp) and I've also added most of that content here. I'll likely continue to add more articles. Part of my reasons to migrate to a static website was so I can start adding content again.
-
-Note: I tried to clean up the migrated content but wordpress evolved quite a bit over the past 20+ years. So there are utf-8 encoding errors that may have slipped in, broken links to websites that no longer exist, bits of information that are no longer relevant/outdated/wrong, etc. If you notice something fixable, let me know. Otherwise enjoy.
-EOF
-`
+export HEADER=$(cat index-header.md)
 
 echo "$HEADER" > _index.md
 
