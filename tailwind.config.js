c80fbb21305b7269a9c4b75a499b2f496bf34ec0
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-08-17 12:19:30 +0200

    refactor: improve tailwind base styles and responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'public/**/*.{html,js,md,css}', 
+    'templates/**/*.{html,js}',
+    'public/**/*.{html,js,md,css}',
   ],
   darkMode: 'media',
   plugins: [],
