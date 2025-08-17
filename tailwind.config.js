7761a076920a50301975922279cd8fc3127c1774
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-08-17 12:05:04 +0200

    fix broken styling and get rid of custom widths and just use px explicitly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'public/**/*.{html,js,md,css}', // adjust to match your files
+    'public/**/*.{html,js,md,css}', 
   ],
-  theme: {
-    extend: {
-      spacing: {
-        430: '430px',
-        800: '800px',
-      },
-    },
-  },
-  darkMode: 'media', // or 'class'
+  darkMode: 'media',
   plugins: [],
 }
